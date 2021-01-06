89cca6d59e36c1e3aefa332a8e64706d3c7bbc1e
MehradNV <mehradnoorani@yahoo.com>
2021-01-07 00:01:50 +0330

    Task5 Q1 code file has been added

---
+++ Task5/Q1/Code/Pele.c
@@ -0,0 +1,38 @@
+/*******************************************************
+This program was created by the
+CodeWizardAVR V3.12 Advanced
+Automatic Program Generator
+© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : Pele
+Version : 
+Date    : 1/6/2021
+Author  : 
+Company : 
+Comments: 
+
+
+Chip type               : ATmega16
+Program type            : Application
+AVR Core Clock frequency: 12.000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 256
+*******************************************************/
+
+#include <mega16.h>
+
+// Declare your global variables here
+
+void main(void)
+{
+// Declare your local variables here
+
+
+while (1)
+      {
+      // Place your code here
+
+      }
+}
